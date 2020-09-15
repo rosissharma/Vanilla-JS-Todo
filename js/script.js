b48518b26d9125fd8850209bfc5a3547143fd878
rs9110 <rs9110@g.rit.edu>
2020-09-15 18:16:34 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,62 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+
+//Functions
+function addTodo(event) {
+    //Prevent form from submitting
+    event.preventDefault();
+
+    //Todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo')
+
+    //List item element
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+
+    //append to div
+    todoDiv.appendChild(newTodo);
+
+    //complete button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('completed-button')
+    todoDiv.appendChild(completedButton);
+
+    //delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteButton.classList.add('delete-button')
+    todoDiv.appendChild(deleteButton);
+
+    //append to list
+    todoList.appendChild(todoDiv);
+
+    //clear todo input value
+    todoInput.value = '';
+}
+
+//delete function
+function deleteCheck(e) {
+    const item = e.target;
+
+    //delete todo
+    if (item.classList[0] === 'delete-button') {
+        const todo = item.parentElement;
+        todo.classList.add('fall');
+        todo.remove();
+    }
+
+    //mark task as completed
+    if (item.classList[0] === 'completed-button') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
